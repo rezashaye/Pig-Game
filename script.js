07dc3356255239bc14edce7ebbd271c2e6638b09
reza shaye <rezashaye2001@gmail.com>
2023-11-04 12:15:10 +0330

    rolling dice functionality - generate,display,add

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 // Selecting elements
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--0'); // little faster
+const score1El = document.getElementById('score--1'); // little faster
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,3 +14,20 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0; // we do not set it in the function , cuz each time we clicked the button the current score will be 0.
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  // 2. Display dice
+  // 3. check for rolled 1: if true, switch to next player
+
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`; // dice image will be loaded
+  if (dice !== 1) {
+    currentScore += dice; // add dice to current score
+    current0El.textContent = currentScore; // change later
+  }
+});
